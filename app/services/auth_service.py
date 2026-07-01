6b315e64eb8d021584f138421eb5d757d51c9e38
Smriti Mulmi <smritimulmi44@gmail.com>
2026-07-01 09:08:18 +0545

    new auth_service

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -12,9 +12,17 @@ def get_user_by_email(db: Session, email: str) -> User | None:
 
 def register_user(db: Session, payload: UserCreate) -> User:
     if get_user_by_email(db, payload.email):
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Email already registered"
+        )
 
-    user = User(email=payload.email, hashed_password=hash_password(payload.password, role = payload.role))
+    user = User(
+        email=payload.email,
+        hashed_password=hash_password(payload.password),
+        role=payload.role
+    )
+
     db.add(user)
     db.commit()
     db.refresh(user)
